e75c8462bb7e0f2bb4a26d0f93bc4958aa032aa7
paulwongx <20178251+paulwongx@users.noreply.github.com>
2025-12-12 20:50:51 -0500

    fix: Fixed sdk bug and passing organizationId

--- packages/polar-kit/src/index.ts
+++ packages/polar-kit/src/index.ts
@@ -1,8 +1,5 @@
 export type { Product } from '@polar-sh/sdk/models/components/product';
-export type {
-  Prices,
-  ProductCreate,
-} from '@polar-sh/sdk/models/components/productcreate';
+export type { ProductCreate } from '@polar-sh/sdk/models/components/productcreate';
 export type { ProductPrice } from '@polar-sh/sdk/models/components/productprice';
 export * from './definitions';
 export { defineConfig } from './utils/define-config';
